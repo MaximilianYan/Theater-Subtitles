2c9cc3f5d8c047318d34d4f19462c12410ef65a9
Максимилиан <max4tim@bk.ru>
2024-12-13 06:35:00 +0300

    css slider smooth control

--- resources/js/index-rangeSliderForm.js
+++ resources/js/index-rangeSliderForm.js
@@ -2,10 +2,17 @@ function rangeSliderMain() {
     let rangeSlider = document.getElementById("rs-range-line");
     let rangeBullet = document.getElementById("rs-bullet");
 
+
+    function calculateValue(x) {
+        return Math.pow(4.773 * 100000, Math.sign(x - 0.5) * Math.pow(Math.abs(x - 0.5), 2.50558));
+    }
+
+
     function showSliderValue() {
-        rangeBullet.innerHTML = rangeSlider.value;
-        var bulletPosition = (rangeSlider.value / rangeSlider.max);
-        rangeBullet.style.left = +rangeSlider.offsetLeft + 11 + (bulletPosition * (rangeSlider.offsetWidth - 22)) + "px";
+        var bulletPart = (+rangeSlider.value / +rangeSlider.max);
+        rangeSlider.calculatedResult = +calculateValue(bulletPart).toFixed(3);
+        rangeBullet.innerHTML = "x" + rangeSlider.calculatedResult;
+        rangeBullet.style.left = +rangeSlider.offsetLeft + 11 + (bulletPart * (rangeSlider.offsetWidth - 22)) + "px";
     }
 
     showSliderValue();
